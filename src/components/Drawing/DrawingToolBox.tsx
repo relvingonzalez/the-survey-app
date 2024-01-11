e231c7022eb4cbe4f49e55d1c95f1b74ad215877
Relvin Gonzalez <otsanllc@gmail.com>
2024-01-11 15:50:04 -0600

    Added tooltips to toolbox buttons

--- src/components/Drawing/DrawingToolBox.tsx
+++ src/components/Drawing/DrawingToolBox.tsx
@@ -8,6 +8,7 @@ import {
   PopoverTarget,
   PopoverDropdown,
   Button,
+  Tooltip,
 } from "@mantine/core";
 import {
   IconCircle,
@@ -33,9 +34,11 @@ export default function DrawingToolBox({
     <Group>
       <Popover width={300} position="bottom" withArrow shadow="md">
         <PopoverTarget>
-          <ActionIcon variant="default" size="xl" aria-label="Gallery">
-            <IconColorPicker style={{ width: rem(20) }} stroke={1.5} />
-          </ActionIcon>
+          <Tooltip label="Select Color">
+            <ActionIcon variant="default" size="xl" aria-label="Gallery">
+              <IconColorPicker style={{ width: rem(20) }} stroke={1.5} />
+            </ActionIcon>
+          </Tooltip>
         </PopoverTarget>
         <PopoverDropdown p="sm">
           <ColorPicker
@@ -61,26 +64,41 @@ export default function DrawingToolBox({
         </PopoverDropdown>
       </Popover>
       <ActionIconGroup>
-        <ActionIcon variant="default" size="xl" aria-label="Gallery">
-          <IconLine style={{ width: rem(20) }} stroke={1.5} />
-        </ActionIcon>
+        <Tooltip label="Line">
+          <ActionIcon variant="default" size="xl" aria-label="Line">
+            <IconLine style={{ width: rem(20) }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
 
-        <ActionIcon variant="default" size="xl" aria-label="Settings">
-          <IconCircle style={{ width: rem(20) }} stroke={1.5} />
-        </ActionIcon>
+        <Tooltip label="Circle">
+          <ActionIcon variant="default" size="xl" aria-label="Circle">
+            <IconCircle style={{ width: rem(20) }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
 
-        <ActionIcon variant="default" size="xl" aria-label="Likes">
-          <IconRectangle style={{ width: rem(20) }} stroke={1.5} />
-        </ActionIcon>
-        <ActionIcon variant="default" size="xl" aria-label="Likes">
-          <IconEaseInOut style={{ width: rem(20) }} stroke={1.5} />
-        </ActionIcon>
-        <ActionIcon variant="default" size="xl" aria-label="Likes">
-          <IconInfoCircleFilled style={{ width: rem(20) }} stroke={1.5} />
-        </ActionIcon>
-        <ActionIcon variant="default" size="xl" aria-label="Likes">
-          <IconServer2 style={{ width: rem(20) }} stroke={1.5} />
-        </ActionIcon>
+        <Tooltip label="Rectangle">
+          <ActionIcon variant="default" size="xl" aria-label="Rectangle">
+            <IconRectangle style={{ width: rem(20) }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
+
+        <Tooltip label="Freehand">
+          <ActionIcon variant="default" size="xl" aria-label="Freehand">
+            <IconEaseInOut style={{ width: rem(20) }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
+
+        <Tooltip label="More Info">
+          <ActionIcon variant="default" size="xl" aria-label="More Info">
+            <IconInfoCircleFilled style={{ width: rem(20) }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
+
+        <Tooltip label="Rack">
+          <ActionIcon variant="default" size="xl" aria-label="Rack">
+            <IconServer2 style={{ width: rem(20) }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
       </ActionIconGroup>
       <Button ml="xl" onClick={onClose}>
         Close
